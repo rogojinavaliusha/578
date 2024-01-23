cdd6e373b4a812d0d943d42a1a8412dbc2019b6e
Valentina Rogojina <rogojinavaliusha@gmail.com>
2024-01-23 02:13:26 +0200

    Создаем массив строк

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿static void Main()
+    {
+        Console.Write("Введите количество строк в массиве: ");
+        int n = int.Parse(Console.ReadLine());
+
+ string[] inputArray = new string[n];
+ 
+
