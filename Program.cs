75d714f55899ee303d0c34e655c138f1e88db61e
Valentina Rogojina <rogojinavaliusha@gmail.com>
2024-01-23 02:14:04 +0200

    Вводим строки с клавиатуры

--- Program.cs
+++ Program.cs
@@ -4,5 +4,11 @@
         int n = int.Parse(Console.ReadLine());
 
  string[] inputArray = new string[n];
- 
+for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите строку #{i + 1}: ");
+            inputArray[i] = Console.ReadLine();
+        }
+        
+
 
